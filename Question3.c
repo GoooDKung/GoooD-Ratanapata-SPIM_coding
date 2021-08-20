0ee5708107db9817c31692e05164d3c09336d064
GoooDKung <47936376+GoooDKung@users.noreply.github.com>
2021-08-20 17:57:11 +0700

    Create Question3.c

---
+++ Question3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+   int A;
+   int B;
+   int C;
+   printf("Enter amout of pyramid");
+   scanf("%d", &A);
+     printf("*\n");
+     if (C>=1){
+      printf("**\n");
+      if (C>=2){
+        printf("***\n");
+        if (C>=3){
+        printf("****\n");
+          if (C>=4){
+        printf("*****\n");
+
+          }
+        }
+      }
+     }
+}
