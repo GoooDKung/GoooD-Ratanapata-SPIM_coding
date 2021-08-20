bc647611b39573fa2919647d952a7903a5588444
GoooDKung <47936376+GoooDKung@users.noreply.github.com>
2021-08-20 17:34:07 +0700

    Create Question4.c

---
+++ Question4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main() {
+   int A;
+   int B;
+   int C;
+   printf("Enter A");
+   scanf("%d", &A);
+   printf("Enter B");
+   scanf("%d", &B);
+  C = A+B;
+  printf("Sums = %d", C);
+   
+   
+  
+  
+
+
+}
